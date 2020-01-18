a91e1a9badf19e58740c6df95c3984ff45f0aaeb
James Finlay <jtfinlay@ualberta.ca>
2020-01-18 14:29:04 -0800

    Support mobile views, that uses different class

--- src/content.js
+++ src/content.js
@@ -1,2 +1,5 @@
 let favicons = document.getElementsByClassName("xA33Gc");
 while (favicons[0]) favicons[0].parentNode.removeChild(favicons[0]);
+
+favicons = document.getElementsByClassName("K7JcSb");
+while (favicons[0]) favicons[0].parentNode.removeChild(favicons[0]);
